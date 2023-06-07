a34cb8681e0b0cbd8646ff6e22169df9dfa18d54
Cyril Vallez <cyril.vallez@gmail.com>
2023-06-07 12:03:36 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=test
+#SBATCH --output=%x-%test.out
+#SBATCH --error=%x-%test.err
+#SBATCH --time=10-00:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=10000
+#SBATCH --partition=nodes
+#SBATCH --gres=gpu:a100:1
+#SBATCH --chdir=/cluster/raid/home/vacy/LLMs
+
+# Verify working directory
+echo $(pwd)
+
+nvidia-smi
+
+nvcc --version
+
+# Initialize the shell to use local conda
+eval "$(conda shell.bash hook)"
+
+# Activate (local) env
+conda activate faiss
+
+python3 hierarchical.py "$@"
+
+conda deactivate
