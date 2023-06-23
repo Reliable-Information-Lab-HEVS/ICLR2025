8ea9a024e464660f559eee1109ebb0203bf2b95f
Cyril Vallez <cyril.vallez@gmail.com>
2023-06-23 12:22:39 +0200

    Update webapp.sh

--- webapp.sh
+++ webapp.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=180G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:6
