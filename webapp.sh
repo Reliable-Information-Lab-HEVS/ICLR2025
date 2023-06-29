2512c9ef3ce3f1aa12212c32e6a85075d3d9d07e
Cyril Vallez <cyril.vallez@gmail.com>
2023-06-29 10:43:12 +0200

    config

--- webapp.sh
+++ webapp.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=120G
+#SBATCH --mem=80G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:4
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
