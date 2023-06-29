16b7ba483f7cfcb698e6bc9e70cc6717372b7dd1
Cyril Vallez <cyril.vallez@gmail.com>
2023-06-29 09:44:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,16 @@ import time
 import loader
 import engine
 import utils
+from huggingface_hub import login
+import os
 
 model = 'star-coder'
+
+token_file = os.path.join(utils.ROOT_FOLDER, '.hf_token.txt')
+with open(token_file, 'r') as file:
+    # Read lines and remove whitespaces
+    token = file.readline().strip()
+
+login(token)
 
 model, tokenizer = loader.load_model_and_tokenizer(model, quantization=False)
