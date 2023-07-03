985b865b6b9b6686bfe62dfa471dabf2d3bb54d3
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-03 10:45:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,21 @@ import agents
 from langchain.agents import initialize_agent, AgentType
 from langchain.chains import LLMChain
 
-llm = agents.HuggingFaceLLM.from_name('star-coder', max_new_tokens=300)
-tools = [agents.Flake8Tool()]
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+# llm = agents.HuggingFaceLLM.from_name('star-coder', max_new_tokens=300)
+# tools = [agents.Flake8Tool()]
+# agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
-agent.run("Write code to multiply 2 numbers, then refactore it according to Flake8.")
+# agent.run("Write code to multiply 2 numbers, then refactore it according to Flake8.")
+
+prompt = "Write code to multiply 2 numbers"
+t0 = time.time()
+model, tokenizer = loader.load_model_and_tokenizer('star-coder')
+dt = time.time() - t0
+print(f'Time to load the model: {dt:.2f} s')
+
+t1 = time.time()
+res = engine.generate_text(model, tokenizer, prompt)
+dt1 = time.time() - t1
+print(f'Time for inference: {dt1:.2f} s')
+print(f'Output: {res}')
+
