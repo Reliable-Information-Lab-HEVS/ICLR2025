1e2f2b08cccd9a85dc7a354b9c306ccd8b37c3ce
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-11 13:59:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -32,39 +32,54 @@ from helpers import utils
 # print(f'Time for inference: {dt1:.2f} s')
 # print(f'Output: {res}')
 
-model_name = 'bloom-560M'
-prompt = "Write code to multiply 2 numbers"
+# model_name = 'bloom-560M'
+# prompt = "Write code to multiply 2 numbers"
 
-model, tokenizer = loader.load_model_and_tokenizer(model_name, device_map='auto')
-print(model.hf_device_map)
-
-t0 = time.time()
-foo = generation.generate_text(model, tokenizer, prompt, num_return_sequences=200, batch_size=100, max_new_tokens=500)
-dt = time.time() - t0
-print(f'Time needed with auto: {dt:.2f} s')
-
-del model, tokenizer
-gc.collect()
-
-model, tokenizer = loader.load_model_and_tokenizer(model_name, device_map='sequential')
-print(model.hf_device_map)
-
-t1 = time.time()
-foo = generation.generate_text(model, tokenizer, prompt, num_return_sequences=200, batch_size=100, max_new_tokens=500)
-dt1 = time.time() - t1
-print(f'Time needed without auto: {dt1:.2f} s')
-
-
-del model, tokenizer
-gc.collect()
-
-model = AutoModelForCausalLM.from_pretrained(loader.DECODER_MODELS_MAPPING[model_name], device_map=None,
-                                                    torch_dtype='auto', load_in_8bit=False)
-model = model.to('cuda:0')
-tokenizer = loader.load_tokenizer(model_name)
+# model, tokenizer = loader.load_model_and_tokenizer(model_name, device_map='auto')
 # print(model.hf_device_map)
 
-t2 = time.time()
-foo = generation.generate_text(model, tokenizer, prompt, num_return_sequences=200, batch_size=100, max_new_tokens=500)
-dt2 = time.time() - t2
-print(f'Time needed using cuda: {dt2:.2f} s')
+# t0 = time.time()
+# foo = generation.generate_text(model, tokenizer, prompt, num_return_sequences=200, batch_size=100, max_new_tokens=500)
+# dt = time.time() - t0
+# print(f'Time needed with auto: {dt:.2f} s')
+
+# del model, tokenizer
+# gc.collect()
+
+# model, tokenizer = loader.load_model_and_tokenizer(model_name, device_map='sequential')
+# print(model.hf_device_map)
+
+# t1 = time.time()
+# foo = generation.generate_text(model, tokenizer, prompt, num_return_sequences=200, batch_size=100, max_new_tokens=500)
+# dt1 = time.time() - t1
+# print(f'Time needed without auto: {dt1:.2f} s')
+
+
+# del model, tokenizer
+# gc.collect()
+
+# model = AutoModelForCausalLM.from_pretrained(loader.DECODER_MODELS_MAPPING[model_name], device_map=None,
+#                                                     torch_dtype='auto', load_in_8bit=False)
+# model = model.to('cuda:0')
+# tokenizer = loader.load_tokenizer(model_name)
+# # print(model.hf_device_map)
+
+# t2 = time.time()
+# foo = generation.generate_text(model, tokenizer, prompt, num_return_sequences=200, batch_size=100, max_new_tokens=500)
+# dt2 = time.time() - t2
+# print(f'Time needed using cuda: {dt2:.2f} s')
+
+
+model_name = 'bloom-7.1B'
+model = AutoModelForCausalLM.from_pretrained(loader.DECODER_MODELS_MAPPING[model_name], device_map=None,
+                                                    torch_dtype='auto', load_in_8bit=False)
+
+param_size = 0
+for param in model.parameters():
+    param_size += param.nelement() * param.element_size()
+buffer_size = 0
+for buffer in model.buffers():
+    buffer_size += buffer.nelement() * buffer.element_size()
+
+size_all_mb = (param_size + buffer_size) / 1024**2
+print('model size: {:.3f}MB'.format(size_all_mb))
