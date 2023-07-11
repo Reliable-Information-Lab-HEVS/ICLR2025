239bc0b921237ee7a7ec4ce39a1260ef543745f0
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-11 13:42:04 +0200

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=20G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:titanv:8
+#SBATCH --gres=gpu:a100:8
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
