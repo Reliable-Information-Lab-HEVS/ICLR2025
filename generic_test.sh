0dc8d423740af481dfc574dfc27acade4a941b9d
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-12 17:08:09 +0200

    Update generic_test.sh

--- generic_test.sh
+++ generic_test.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=40G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=1G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:2
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
