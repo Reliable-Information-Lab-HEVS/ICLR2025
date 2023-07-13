3f705fee681af1f6b4b0742441bb26271ce5bb5c
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-13 10:39:13 +0200

    Finalizes loader

--- engine/loader.py
+++ engine/loader.py
@@ -469,15 +469,12 @@ def load_model(model_name: str, quantization: bool = False, device_map: str | No
         gpu_memory = 0.8 * gpu_memory
         gpu_number = torch.cuda.device_count()
 
-        # Compute the minimum number of gpus needed. Note that we allow some flexibility: if the remainder
-        # is very small (less than 3% of gpu_memory), we don't use an additional gpu for that remainder
-        if rough_model_size_estimate % gpu_memory < 0.03 * gpu_memory:
-            min_gpu_needed = rough_model_size_estimate // gpu_memory
-        else:
-            min_gpu_needed = rough_model_size_estimate // gpu_memory + 1
-
-        # Can be 0 for very small models for which the expected size is only 0.03*gpu_memory or less
-        min_gpu_needed = min_gpu_needed if min_gpu_needed > 0 else 1
+        # Compute the minimum number of gpus needed
+        min_gpu_needed = rough_model_size_estimate // gpu_memory
+        # Heuristic: if the remainder is smaller than 2% of each gpu_memory, do not add a gpu and distill
+        # the small excess between existing gpus
+        if rough_model_size_estimate % gpu_memory >= (0.02 * gpu_memory) * min_gpu_needed:
+            min_gpu_needed += 1
 
 
         if min_gpu_needed > gpu_number:
