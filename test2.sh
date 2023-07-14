5e4f16e20ae681960a67c9d9958b21066c3b93b9
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-14 17:31:09 +0200

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=30G
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=50G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:2
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
