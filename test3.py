4c549673c211f5c1011e521a5bbb730a7f60b410
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-17 10:00:26 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -7,11 +7,23 @@ from engine import generation
 from engine import stopping
 import time
 
-model = engine.HFModel('codegen-16B', gpu_rank=0, device_map='auto')
+prompt = "# Write a python function to multiply 2 numbers"
+
+# model = engine.HFModel('codegen-16B', gpu_rank=0, device_map='auto')
+# print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
+# print(model.device_map)
+
+# t0 = time.time()
+# out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=100,
+#             stopping_patterns=stopping.CODE_STOP_PATTERNS)
+# dt = time.time() - t0
+# print(f'After generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
+# print(f'Time for generation: {dt:.2f} s')
+
+
+model = engine.HFModel('codegen-16B', gpu_rank=0)
 print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
 print(model.device_map)
-
-prompt = "# Write a python function to multiply 2 numbers"
 
 t0 = time.time()
 out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=100,
