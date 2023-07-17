19a3c09c2351004ab15e35cbbe25d6927e23414f
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-17 10:19:23 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -21,13 +21,18 @@ prompt = "# Write a python function to multiply 2 numbers"
 # print(f'Time for generation: {dt:.2f} s')
 
 
-model = engine.HFModel('codegen-16B', gpu_rank=0)
+# model = engine.HFModel('codegen-16B', gpu_rank=0)
+# print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
+# print(model.device_map)
+model = AutoModelForCausalLM.from_pretrained('Salesforce/codegen-16B-mono', torch_dtype=torch.float16).to('cuda:0')
+tokenizer = AutoTokenizer.from_pretrained('Salesforce/codegen-16B-mono')
 print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
-print(model.device_map)
 
 t0 = time.time()
-out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=100,
-            stopping_patterns=stopping.CODE_STOP_PATTERNS)
+# out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=100,
+            # stopping_patterns=stopping.CODE_STOP_PATTERNS)
+out = generation.generate_text(model, tokenizer, prompt, max_new_tokens=512, num_return_sequences=200, batch_size=100,
+                               stopping_patterns=stopping.CODE_STOP_PATTERNS)
 dt = time.time() - t0
 print(f'After generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
 print(f'Time for generation: {dt:.2f} s')
