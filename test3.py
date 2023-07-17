7b1b60a51ceb06bd01f69b10ac7758a88663ed06
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-17 16:19:16 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -9,34 +9,16 @@ import time
 
 prompt = "# Write a python function to multiply 2 numbers"
 
-# model = engine.HFModel('codegen-16B', gpu_rank=0, device_map='auto')
-# print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
-# print(model.device_map)
-
-# t0 = time.time()
-# out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=100,
-#             stopping_patterns=stopping.CODE_STOP_PATTERNS)
-# dt = time.time() - t0
-# print(f'After generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
-# print(f'Time for generation: {dt:.2f} s')
-
-
-# model = engine.HFModel('codegen-16B', gpu_rank=0)
-# print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
-# print(model.device_map)
-model = AutoModelForCausalLM.from_pretrained('Salesforce/codegen-16B-mono', torch_dtype=torch.float16, device_map='balanced')
-print(model.hf_device_map)
-tokenizer = AutoTokenizer.from_pretrained('Salesforce/codegen-16B-mono')
-for i in range(torch.cuda.device_count()):
-    print(f'Before generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.2f} GB')
+model = engine.HFModel('star-coder', gpu_rank=0)
+print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
+print(model.device_map)
 
 t0 = time.time()
-# out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=100,
-            # stopping_patterns=stopping.CODE_STOP_PATTERNS)
-out = generation.generate_text(model, tokenizer, prompt, max_new_tokens=512, num_return_sequences=200, batch_size=16,
-                               stopping_patterns=None)
+out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=200,
+            stopping_patterns=stopping.CODE_STOP_PATTERNS)
 dt = time.time() - t0
-for i in range(torch.cuda.device_count()):
-    print(f'After generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.2f} GB')
+print(f'After generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
 print(f'Time for generation: {dt:.2f} s')
+
+
 
