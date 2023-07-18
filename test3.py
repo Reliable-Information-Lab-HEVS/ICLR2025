e368163fdc7ceb371e98842aada314c8bed84987
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-18 11:25:43 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -8,16 +8,26 @@ from engine import stopping
 import time
 
 prompt = "# Write a python function to multiply 2 numbers"
+max_tokens = 512
+batch_size = 200
 
-model = engine.HFModel('star-coder', gpu_rank=0)
-print(f'Before generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
+model = engine.HFModel('bloom-560M', gpu_rank=0, device_map='balanced_low_0')
+for i in range(torch.cuda.device_count()):
+    print(f'Before generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.2f} GB')
 print(model.device_map)
 
+input_size = model.tokenizer.encode(prompt, return_tensors='pt').shape[1]
+multiplier = 2 if (model.dtype == torch.bfloat16 or model.dtype == torch.float16) else 4
+inferred_mem_size = batch_size * (input_size + max_tokens) * model.tokenizer.vocab_size * multiplier / 1024**3
+
 t0 = time.time()
-out = model(prompt, max_new_tokens=512, num_return_sequences=200, batch_size=200,
-            stopping_patterns=stopping.CODE_STOP_PATTERNS)
+out = model(prompt, max_new_tokens=max_tokens, num_return_sequences=200, batch_size=batch_size,
+            stopping_patterns=None)
 dt = time.time() - t0
-print(f'After generation: {(torch.cuda.max_memory_allocated(0) / 1024**3):.2f} GB')
+for i in range(torch.cuda.device_count()):
+    print(f'After generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.2f} GB')
+
+print(f'According to calculation, memory should be {inferred_mem_size:.5f} GB')
 print(f'Time for generation: {dt:.2f} s')
 
 
