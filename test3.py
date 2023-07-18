6ac4f00157a58d0c3fb93f284676ba4421ae2195
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-18 11:35:23 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -13,7 +13,7 @@ batch_size = 200
 
 model = engine.HFModel('bloom-560M', gpu_rank=0, device_map='balanced_low_0')
 for i in range(torch.cuda.device_count()):
-    print(f'Before generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.2f} GB')
+    print(f'Before generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.5f} GB')
 print(model.device_map)
 
 input_size = model.tokenizer.encode(prompt, return_tensors='pt').shape[1]
@@ -24,8 +24,14 @@ t0 = time.time()
 out = model(prompt, max_new_tokens=max_tokens, num_return_sequences=200, batch_size=batch_size,
             stopping_patterns=None)
 dt = time.time() - t0
+
+size_out = model.tokenizer.encode(out, return_tensors='pt').shape[1]
+if size_out != (input_size + max_tokens):
+    print(f'Early stopping of generation after {size_out} tokens total.')
+    inferred_mem_size = batch_size * size_out * model.tokenizer.vocab_size * multiplier / 1024**3
+
 for i in range(torch.cuda.device_count()):
-    print(f'After generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.2f} GB')
+    print(f'After generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.5f} GB')
 
 print(f'According to calculation, memory should be {inferred_mem_size:.5f} GB')
 print(f'Time for generation: {dt:.2f} s')
