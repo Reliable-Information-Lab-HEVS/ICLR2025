4f9850cf5f45b768e2b21bca676e292de9673792
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-19 15:11:41 +0200

    Update test4.py

--- test4.py
+++ test4.py
@@ -81,11 +81,11 @@ max_tokens = 512
 batch_size = 200
 
 model = engine.HFModel('bloom-560M', gpu_rank=0, device_map='balanced_low_0')
-model.forward = new_forward
+model.model.forward = new_forward
 print(model.device_map)
 print(model.input_device)
-input_size = model.tokenizer.encode(prompt, return_tensors='pt').shape[1]
-print(f'Input sequence size: {input_size}')
+# input_size = model.tokenizer.encode(prompt, return_tensors='pt').shape[1]
+# print(f'Input sequence size: {input_size}')
 
 for i in range(torch.cuda.device_count()):
     print(f'Before generation gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.5f} GB')
@@ -93,10 +93,10 @@ for i in range(torch.cuda.device_count()):
 for i in range(torch.cuda.device_count()):
     torch.cuda.reset_peak_memory_stats(device=i)
 
-# out1 = model(prompt, num_return_sequences=200, max_new_tokens=max_tokens, seed=1)
-input_ids = model.tokenizer.encode(prompt, return_tensors='pt')
-large_input, _ = model.model._expand_inputs_for_generation(expand_size=200, input_ids=input_ids)
-out1 = model.model(large_input)
+out1 = model(prompt, num_return_sequences=200, max_new_tokens=max_tokens, seed=1)
+# input_ids = model.tokenizer.encode(prompt, return_tensors='pt')
+# large_input, _ = model.model._expand_inputs_for_generation(expand_size=200, input_ids=input_ids)
+# out1 = model.model(large_input)
 
 for i in range(torch.cuda.device_count()):
     print(f'After generation with trick gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.5f} GB')
@@ -107,13 +107,13 @@ model = engine.HFModel('bloom-560M', gpu_rank=0, device_map='balanced_low_0')
 for i in range(torch.cuda.device_count()):
     torch.cuda.reset_peak_memory_stats(device=i)
 
-# out2 = model(prompt, num_return_sequences=200, max_new_tokens=max_tokens, seed=1)
-input_ids = model.tokenizer.encode(prompt, return_tensors='pt')
-large_input, _ = model.model._expand_inputs_for_generation(expand_size=200, input_ids=input_ids)
-out2 = model.model(large_input)
+out2 = model(prompt, num_return_sequences=200, max_new_tokens=max_tokens, seed=1)
+# input_ids = model.tokenizer.encode(prompt, return_tensors='pt')
+# large_input, _ = model.model._expand_inputs_for_generation(expand_size=200, input_ids=input_ids)
+# out2 = model.model(large_input)
 
 for i in range(torch.cuda.device_count()):
     print(f'After generation without trick gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.5f} GB')
 
-# print(f'The outputs are similar: {out1 == out2}')
-print(f'The outputs are similar: {torch.all(out1 == out2)}')
+print(f'The outputs are similar: {out1 == out2}')
+# print(f'The outputs are similar: {torch.all(out1 == out2)}')
