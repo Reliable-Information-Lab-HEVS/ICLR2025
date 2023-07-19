0d567a863169e2595b39d67a1bd8773219cd7ff1
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-19 11:29:54 +0200

    Update test4.py

--- test4.py
+++ test4.py
@@ -93,7 +93,7 @@ for i in range(torch.cuda.device_count()):
 for i in range(torch.cuda.device_count()):
     torch.cuda.reset_peak_memory_stats(device=i)
 
-out1 = model(prompt, num_return_sequences=200, max_new_tokens=10, seed=1)
+out1 = model(prompt, num_return_sequences=200, max_new_tokens=max_tokens, seed=1)
 
 for i in range(torch.cuda.device_count()):
     print(f'After generation with trick gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.5f} GB')
@@ -104,7 +104,7 @@ model = engine.HFModel('bloom-560M', gpu_rank=0, device_map='balanced_low_0')
 for i in range(torch.cuda.device_count()):
     torch.cuda.reset_peak_memory_stats(device=i)
 
-out2 = model(prompt, num_return_sequences=200, max_new_tokens=10, seed=1)
+out2 = model(prompt, num_return_sequences=200, max_new_tokens=max_tokens, seed=1)
 
 for i in range(torch.cuda.device_count()):
     print(f'After generation without trick gpu {i}: {(torch.cuda.max_memory_allocated(i) / 1024**3):.5f} GB')
