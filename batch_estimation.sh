e6f2ec6ce6e6c6446a9a277b432709bdbe11cf5c
Cyril Vallez <cyril.vallez@gmail.com>
2023-07-21 13:47:02 +0200

    Create batch_estimation.sh

---
+++ batch_estimation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=batch_estimations
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=10-00:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=80G
+#SBATCH --partition=nodes
+#SBATCH --gres=gpu:a100:2
+#SBATCH --chdir=/cluster/raid/home/vacy/LLMs
+
+# Initialize the shell to use local conda
+eval "$(conda shell.bash hook)"
+
+# Activate (local) env
+conda activate llm
+
+python3 -u memory_estimator.py "$@"
+
+conda deactivate
