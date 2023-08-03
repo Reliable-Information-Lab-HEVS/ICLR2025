fbeee63e892f86964268ada223da1fb2849400bc
Cyril Vallez <cyril.vallez@gmail.com>
2023-08-03 10:59:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -50,11 +50,11 @@ prompt = model.tokenizer.batch_decode(large_tokens[:, :input_size], skip_special
 print(f'Batch size: {model.infer_best_batch_size(input_size, max_new_tokens, 200)}')
 try:
     foo = model(prompt, num_return_sequences=200, max_new_tokens=max_new_tokens, seed=1,
-                batch_size=50)
+                batch_size=40)
 except torch.cuda.OutOfMemoryError:
     torch.cuda.empty_cache()
     foo = model(prompt, num_return_sequences=200, max_new_tokens=max_new_tokens, seed=1,
-                batch_size=40)
+                batch_size=30)
 
 gpu_mem = torch.cuda.get_device_properties(0).total_memory / 1024**3
 for i in range(torch.cuda.device_count()):
