7d2aae52abd9f9fdbc57185464a7f59ee67a89c0
Cyril Vallez <cyril.vallez@gmail.com>
2023-08-10 15:42:41 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -21,21 +21,21 @@ print(f'Low cpu option: {dt1:.2f}s')
 del model
 
 t2 = time.time()
-model = AutoModelForCausalLM.from_pretrained(model_name, dtype=torch.bfloat16)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
 dt2 = time.time() - t2
 print(f'dtype16 option: {dt2:.2f}s')
 
 del model
 
 t3 = time.time()
-model = AutoModelForCausalLM.from_pretrained(model_name, dtype=torch.bfloat16, low_cpu_mem_usage=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True)
 dt3 = time.time() - t3
 print(f'Low cpu option + dtype16: {dt3:.2f}s')
 
 del model
 
 t4 = time.time()
-model = AutoModelForCausalLM.from_pretrained(model_name, dtype=torch.bfloat16, low_cpu_mem_usage=True).cuda()
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True).cuda()
 dt4 = time.time() - t4
 print(f'Low cpu option + dtype16 + gpu: {dt4:.2f}s')
 
