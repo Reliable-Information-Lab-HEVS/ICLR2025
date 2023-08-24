faf0ec0cd0c8684a718cd47edd48d3b8bda4bf9e
Cyril Vallez <cyril.vallez@gmail.com>
2023-08-24 20:02:33 +0200

    Update human_eval.py

--- human_eval.py
+++ human_eval.py
@@ -426,21 +426,14 @@ if __name__ == '__main__':
     mode_iter = (mode,)*len(small_models)
     use_context_iter = (use_context,)*len(small_models)
 
-    if instruct:
-        target = lambda model: human_eval_instruct(model, mode, use_context)
-    else:
-        target = lambda model: human_eval(model, mode)
-
     # Run all models that fit on a single gpu in parallel using all gpus
     # Use ProcessPoolExecutor() instead of mp.Pool() because it is slightly more convenient
     # with mp.Pool(processes=num_gpus, initializer=utils.set_cuda_visible_device_of_subprocess) as pool:
     with ProcessPoolExecutor(max_workers=num_gpus, initializer=utils.set_cuda_visible_device_of_subprocess) as pool:
-        # if instruct:
-            # pool.map(human_eval_instruct, small_models, mode_iter, use_context_iter, chunksize=1)
-        # else:
-            # pool.map(human_eval, small_models, mode_iter, chunksize=1)
-        # pool.map(target, small_models, chunksize=1)
-        list(pool.map(lambda x: print('Fuck you'), [i for i in range(10)]))
+        if instruct:
+            _ = list(pool.map(human_eval_instruct, small_models, mode_iter, use_context_iter, chunksize=1))
+        else:
+            _ = list(pool.map(human_eval, small_models, mode_iter, chunksize=1))
 
     if big_models:
         for model in large_models:
