0595064daa798224057c4b1b67eb0297e35a367f
Cyril Vallez <cyril.vallez@gmail.com>
2023-08-25 17:45:24 +0200

    Update memory_estimator.sh

--- memory_estimator.sh
+++ memory_estimator.sh
@@ -5,9 +5,9 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=80G
+#SBATCH --mem=120G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100:4
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
@@ -16,6 +16,6 @@ eval "$(conda shell.bash hook)"
 # Activate (local) env
 conda activate llm
 
-python3 -u memory_estimator.py "$@"
+python3 memory_estimator.py "$@"
 
 conda deactivate
