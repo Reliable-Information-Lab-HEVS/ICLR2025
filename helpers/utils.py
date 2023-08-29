8f045beced887d0dc5ac53fdb78226aee7b7875e
Cyril Vallez <cyril.vallez@gmail.com>
2023-08-29 12:32:28 +0200

    test

--- helpers/utils.py
+++ helpers/utils.py
@@ -517,3 +517,11 @@ def dispatch_jobs(model_names, model_footprints, num_gpus, target_func, *func_ar
     # Sleep until all processes are finished (they have all been scheduled at this point)
     for process in processes:
         process.join()
+
+
+def target(foo, bar):
+    import torch
+    print(os.environ['CUDA_VISIBLE_DEVICES'])
+    print(f'Number of gpus seen by torch: {torch.cuda.device_count()}')
+    time.sleep(5)
+    print('Done!')
