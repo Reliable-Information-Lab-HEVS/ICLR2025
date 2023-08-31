d4d9d12ddedb2bdbb709949a358014299752a317
Cyril Vallez <cyril.vallez@gmail.com>
2023-08-31 11:26:58 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -8,7 +8,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model_name = 'facebook/opt-13b'
 
-max_memory = {0: '8GiB', 1: '30GiB'}
+max_memory = {0: '4GiB', 1: '30GiB'}
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map='balanced',
                                              max_memory=max_memory)
 
