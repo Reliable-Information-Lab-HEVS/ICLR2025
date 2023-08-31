c6ba45a0886f64efc6ac3b2d01362296a1b218bc
Cyril Vallez <cyril.vallez@gmail.com>
2023-08-31 10:59:34 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -12,6 +12,9 @@ max_memory = {0: '8GiB', 1: '30GiB'}
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map='balanced',
                                              max_memory=max_memory)
 
+print(f'GPU 0: {torch.cuda.memory_allocated(0) / 1024**3:.2f} GiB')
+print(f'GPU 1: {torch.cuda.memory_allocated(1) / 1024**3:.2f} GiB')
+
 print(model.hf_device_map)
 
 
