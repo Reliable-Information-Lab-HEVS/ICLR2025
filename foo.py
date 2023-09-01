ceeef6da5d7733a1825526bba611f84ad6d937ea
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-01 14:06:50 +0200

    Update foo.py

--- foo.py
+++ foo.py
@@ -5,7 +5,8 @@ import torch
 from concurrent.futures import ProcessPoolExecutor
 
 # from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForMaskedLM, AutoModelForSeq2SeqLM
-from transformers import PreTrainedModel#, PreTrainedTokenizerBase
+# from transformers import PreTrainedModel
+from transformers import PreTrainedTokenizerBase
 
 # from engine import loader
 # from engine import loader2
