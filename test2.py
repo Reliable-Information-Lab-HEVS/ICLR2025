e560f28384dc452aab70b525fbe4706b831cfa04
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-12 19:11:28 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -42,7 +42,7 @@ from helpers import utils, datasets
 
 model_name = 'bloom-560M'
 
-# model = engine.HFModel(model_name)
+model = engine.HFModel(model_name)
 # print(model.get_gpu_memory_footprint())
 
 from transformers import AutoModelForCausalLM
