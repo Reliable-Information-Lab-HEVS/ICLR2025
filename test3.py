a2c0c04f60970e1409028903af4c66e8e5be8e3c
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-13 09:28:54 +0200

    test

--- test3.py
+++ test3.py
@@ -3,10 +3,12 @@ import torch
 from accelerate import init_empty_weights, load_checkpoint_and_dispatch, infer_auto_device_map
 import time
 
-model_name = 'facebook/opt-13b'
+import engine
 
-max_memory = {0: '3GiB', 1: '25GiB', 2:'25GiB'}
-device_map = 'balanced'
+# model_name = 'facebook/opt-13b'
+
+# max_memory = {0: '3GiB', 1: '25GiB', 2:'25GiB'}
+# device_map = 'balanced'
 # t0 = time.time()
 # model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map=device_map,
 #                                              max_memory=max_memory)
@@ -19,23 +21,31 @@ device_map = 'balanced'
 # print(model.hf_device_map)
 
 
-t0 = time.time()
-config = AutoConfig.from_pretrained(model_name, torch_dtype=torch.float16)
-with init_empty_weights():
-    model = AutoModelForCausalLM.from_config(config)
-    model.tie_weights()
-    device_map = infer_auto_device_map(model, max_memory=max_memory, dtype=torch.float16)
-dt0 = time.time() - t0
-print(f'Time to compute device_map: {dt0:.2f} s')
 
-t1 = time.time()
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map=device_map)
-dt1 = time.time() - t1
-print(f'Time to load model: {dt1:.2f} s')
 
-print(f'Total time: {dt0 + dt1:.2f} s')
 
-for i in range(torch.cuda.device_count()):
-    print(f'GPU {i}: {torch.cuda.memory_allocated(i) / 1024**3:.2f} GiB')
+# t0 = time.time()
+# config = AutoConfig.from_pretrained(model_name, torch_dtype=torch.float16)
+# with init_empty_weights():
+#     model = AutoModelForCausalLM.from_config(config)
+#     model.tie_weights()
+#     device_map = infer_auto_device_map(model, max_memory=max_memory, dtype=torch.float16)
+# dt0 = time.time() - t0
+# print(f'Time to compute device_map: {dt0:.2f} s')
 
-print(model.hf_device_map)
+# t1 = time.time()
+# model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map=device_map)
+# dt1 = time.time() - t1
+# print(f'Time to load model: {dt1:.2f} s')
+
+# print(f'Total time: {dt0 + dt1:.2f} s')
+
+# for i in range(torch.cuda.device_count()):
+#     print(f'GPU {i}: {torch.cuda.memory_allocated(i) / 1024**3:.2f} GiB')
+
+# print(model.hf_device_map)
+
+
+model_name = 'bloom-176B'
+model = engine.HFModel(model_name, quantization_8bits=True)
+print(model.get_gpu_memory_footprint())
--- test3.sh
+++ test3.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=50G
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=80G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:3
+#SBATCH --gres=gpu:a100:6
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
