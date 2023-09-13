c6f75481e14395f0a2613b0932e3498c39a2badc
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-13 15:48:51 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -17,31 +17,32 @@ from engine import loader
 from helpers import utils, datasets
 
 # model_name = 'llama2-7B'
+model_name = 'code-llama-7B-python'
 
-# t0 = time.time()
-# model = engine.HFModel(model_name)
-# dt = time.time() - t0
-# print(f'Time for loading: {dt:.2f} s')
+t0 = time.time()
+model = engine.HFModel(model_name)
+dt = time.time() - t0
+print(f'Time for loading: {dt:.2f} s')
 
-# dataset = datasets.HumanEval()
-# prompt = dataset[0]['prompt']
+dataset = datasets.HumanEval()
+prompt = dataset[0]['prompt']
 
-# t0 = time.time()
-# out1 = model(prompt, max_new_tokens=512, do_sample=False, batch_size=1, num_return_sequences=1,
-#              stopping_patterns=True)
+t0 = time.time()
+out1 = model(prompt, max_new_tokens=512, do_sample=False, batch_size=1, num_return_sequences=1,
+             stopping_patterns=True)
 
-# print(out1)
-# print('\n\n')
+print(out1)
+print('\n\n')
 
-# out2 = model(prompt.strip(), max_new_tokens=512, do_sample=False, batch_size=1, num_return_sequences=1,
-#              stopping_patterns=True)
-# dt = time.time() - t0
+out2 = model(prompt.strip(), max_new_tokens=512, do_sample=False, batch_size=1, num_return_sequences=1,
+             stopping_patterns=True)
+dt = time.time() - t0
 
-# print(out2)
+print(out2)
 
-# print('\n\n')
+print('\n\n')
 
-# print(f'Time for 2 inferences: {dt:.2f} s')
+print(f'Time for 2 inferences: {dt:.2f} s')
 
 
 
@@ -64,23 +65,23 @@ from helpers import utils, datasets
 # foo = loader.estimate_model_gpu_footprint('bloom-560M')
 
 
-LARGE_MODELS = (
-    'gpt-neoX-20B',
-    'opt-30B',
-    'opt-66B',
-    'llama2-70B',
-    'llama2-70B-chat',
-    'bloom-176B',
-)
+# LARGE_MODELS = (
+#     'gpt-neoX-20B',
+#     'opt-30B',
+#     'opt-66B',
+#     'llama2-70B',
+#     'llama2-70B-chat',
+#     'bloom-176B',
+# )
 
-model_footprints = []
-for model in LARGE_MODELS:
-    # Override quantization for bloom because it's too big
-    if model == 'bloom-176B':
-        gpu_needed, _ = loader.estimate_model_gpu_footprint(model, quantization_8bits=True,
-                                                            quantization_4bits=False)
-    else:
-        gpu_needed, _ = loader.estimate_model_gpu_footprint(model)
-    model_footprints.append(gpu_needed)
+# model_footprints = []
+# for model in LARGE_MODELS:
+#     # Override quantization for bloom because it's too big
+#     if model == 'bloom-176B':
+#         gpu_needed, _ = loader.estimate_model_gpu_footprint(model, quantization_8bits=True,
+#                                                             quantization_4bits=False)
+#     else:
+#         gpu_needed, _ = loader.estimate_model_gpu_footprint(model)
+#     model_footprints.append(gpu_needed)
 
-print(model_footprints)
+# print(model_footprints)
