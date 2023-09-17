0a840b6770c6954be100b8c7fd41442ddf777db1
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-17 16:18:05 +0200

    Update human_eval.sh

--- human_eval.sh
+++ human_eval.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=300G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:8
+#SBATCH --gres=gpu:a100:§
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
