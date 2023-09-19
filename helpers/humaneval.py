33b523c158923bf254e4e12c990f512abc2baaf3
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-19 18:22:30 +0200

    Refactore humaneval

--- helpers/humaneval.py
+++ helpers/humaneval.py
@@ -264,25 +264,153 @@ def evaluate_pass_at_k(result_file: str, k: list[int] = [1, 10, 100]) -> dict:
 
 
 
-def all_passes_at_k(k: list[int] = [1, 10, 100]) -> list[dict]:
-    """Compute the pass@k for all available HumanEval benchmarks.
+def find_error_causes(result_file: str) -> tuple[list, list]:
+    """Retrieve the different causes of error (exception types) for which the code failed.
 
     Parameters
     ----------
-    k : list[int], optional
-        The different `k` for pass@k, by default [1, 10, 100]
+    result_file : str
+        File where the results are stored.
 
     Returns
     -------
-    list[dict]
-        A list of dictionary containing all details about the given benchmark, and the pass@k.
+    tuple[list, list]
+        The different exception types, and their proportion.
     """
 
-    files = extract_all_filenames(category='results')
+    dics = utils.load_jsonl(result_file)
+    results = [dic['exception'] if dic['exception'] is not None else 'passed' for dic in dics]
+    
+    errors, count = np.unique(results, return_counts=True)
+    # Make it the proportion instead of raw numbers
+    count = count / len(results)
 
-    # Compute the pass at k for every result file
-    passes = []
-    for file in files:
+    return errors.tolist(), count.tolist()
+
+
+
+def find_best_temperature_file(folder: str, k: int = 1, greedy: bool = False) -> str:
+    """Find the temperature (file) corresponding to the best pass@k inside the `folder`.
+
+    Parameters
+    ----------
+    folder : str
+        Folder in which we store the results corresponding to different temperatures.
+    k : int, optional
+        The `k` in pass@k, by default 1
+    greedy : bool, optional
+        Whether we are computing pass@k for greedy decoding or not, by default False
+
+    Returns
+    -------
+    str
+        The file corresponding to the results with the nest temperature.
+    """
+
+    if greedy and k != 1:
+        raise ValueError('We can only compute pass@1 for greedy decoding.')
+    
+    greedy_file = os.path.join(folder, 'temperature_0.0.jsonl')
+    if greedy:
+        if not os.path.exists(greedy_file):
+            raise RuntimeError('Looks like we never computed completions with temperature 0.')
+        else:
+            return greedy_file
+    
+    else:
+        temperature_files = [os.path.join(folder, file) for file in os.listdir(folder) \
+                              if file.startswith('temperature') and file != greedy_file]
+
+        passes = []
+        for file in temperature_files:
+            try:
+                passes.append(evaluate_pass_at_k(file, [k])[f'pass@{k}'])
+            except IndexError:
+                raise RuntimeError(f'The k you provided is larger than the number of sequences generated in {file}')
+            
+        return temperature_files[np.argmax(passes)]
+    
+
+
+def _get_default_dtype(model_name: str) -> str:
+    """Return the default dtype used by a given model.
+    """
+    if model_name == 'bloom-176B':
+        return 'int8'
+    else:
+        return str(loader.get_model_dtype(model_name)).split('.', 1)[1]
+
+
+
+def find_folders_with_dtype(benchmark: str, dtype: str = 'default') -> list[str]:
+    """Return the folders corresponding to the given `benchmark` and `dtype`, in which we can find the 
+    results with different temperatures.
+
+    Parameters
+    ----------
+    benchmark : str
+        The name of the benchmark, i.e. `HumanEval_default`, or `HumanEval_generation`.
+    dtype : str, optional
+        A precise dtype to use for fetching the results, by default 'default'
+
+    Returns
+    -------
+    list[str]
+        All the folders corresponding to the given `benchmark` and `dtype`.
+    """
+
+    assert 'HumanEval' in benchmark, 'The benchmark name is not correct'
+
+    dtypes = ['default', 'int8', 'int4']
+    if dtype not in dtypes:
+        raise ValueError(f'Dtype must be in {*dtypes,}')
+
+    benchmark_path = os.path.join(utils.RESULTS_FOLDER, benchmark, 'results')
+    
+    folders = []
+    for model in os.listdir(benchmark_path):
+        if not model.startswith('.'):
+            model_path = os.path.join(benchmark_path, model)
+            dtype_dirs = [x for x in os.listdir(model_path) if not x.startswith('.')]
+            # Get the default dtype of the current model
+            actual_dtype = _get_default_dtype(model) if dtype == 'default' else dtype
+            # If dtype is present in the list of dtype, add the folder to the list
+            if actual_dtype in dtype_dirs:
+                folders.append(os.path.join(model_path, actual_dtype))
+
+    return folders
+
+
+
+def benchmark_passes_at_k_and_error_causes(benchmark: str, dtype: str = 'default', k: int = 1,
+                                           greedy: bool = True, to_df: bool = False) -> list[dict] | pd.DataFrame:
+    """Compute the pass@k (for the best temperature) and error causes for all models corresponding to the given
+    `benchmark` and `dtype`. 
+
+    Parameters
+    ----------
+    benchmark : str
+        The name of the benchmark, i.e. `HumanEval_default`, or `HumanEval_generation`.
+    dtype : str, optional
+        A precise dtype to use for fetching the results, by default 'default'
+    k : int, optional
+        The `k` in pass@k, by default 1
+    greedy : bool, optional
+        Whether we are computing pass@k for greedy decoding or not, by default True
+    to_df : bool, optional
+        Whether to convert to DataFrame or not, by default False
+
+    Returns
+    -------
+    list[dict] | pd.DataFrame
+        A list of dictionary or DataFrame containing all interesting attributes for each model in the benchmark.
+    """
+    
+    folders = find_folders_with_dtype(benchmark, dtype=dtype)
+
+    results = []
+    for folder in folders:
+        file = find_best_temperature_file(folder, k=k, greedy=greedy)
 
         attributes = parse_filename(file)
         # Discard unnecessary keys
@@ -295,68 +423,112 @@ def all_passes_at_k(k: list[int] = [1, 10, 100]) -> list[dict]:
         model_size = loader.ALL_MODELS_PARAMS[model]
         model_family = loader.ALL_MODELS_FAMILY[model]
 
-        pass_at_k = evaluate_pass_at_k(file, k)
-        passes.append({'model_size': model_size, 'model_family': model_family, **attributes, **pass_at_k})
+        pass_at_k_ = evaluate_pass_at_k(file, [k])
+        error_causes, error_proportions = find_error_causes(file)
+        results.append({'model_size': model_size, 'model_family': model_family, 'error_causes': error_causes,
+                       'error_proportions': error_proportions, **attributes, **pass_at_k_})
+        
+    # sort them according to family, then size, then name (for model with same family and size)
+    results = sorted(results, key=lambda x: (x['model_family'], x['model_size'], x['model']))
 
-    return passes
+    if to_df:
+        return pd.DataFrame.from_records(results)
+    else:
+        return results
 
 
 
-def model_wise_pass_at_k(k: int = 1, nan_values: str | None = '-') -> pd.DataFrame:
-    """Compute the pass@k model-wise for all benchmarks, i.e. compare the benchmarks for each model.
+def all_passes_at_k_and_error_causes(dtype: str = 'default', k: int = 1, greedy: bool = True,
+                                         to_df: bool = False) -> dict[str, list[dict] | pd.DataFrame]:
+    """Compute the pass@k and error causes (for the best temperature) for all models and all benchmarks
+    corresponding to the given `dtype`. 
 
     Parameters
     ----------
+    dtype : str, optional
+        A precise dtype to use for fetching the results, by default 'default'
     k : int, optional
         The `k` in pass@k, by default 1
+    greedy : bool, optional
+        Whether we are computing pass@k for greedy decoding or not, by default True
+    to_df : bool, optional
+        Whether to convert to DataFrame or not, by default False
+
+    Returns
+    -------
+    dict[str, list[dict] | pd.DataFrame]
+        A dictionary mapping each benchmark to its results.
+    """
+
+    benchmarks = [x for x in os.listdir(utils.RESULTS_FOLDER) if not x.startswith('.') and 'HumanEval' in x]
+    return {bench: benchmark_passes_at_k_and_error_causes(bench, dtype=dtype, k=k, greedy=greedy, to_df=to_df) \
+            for bench in benchmarks}
+
+
+def model_wise_pass_at_k(dtype: str = 'default', k: int = 1, greedy: bool = True,
+                             nan_values: str | None = '-') -> pd.DataFrame:
+    """Comparison of the pass@k for all the models and benchmarks, and given `dtype`.
+
+    Parameters
+    ----------
+    dtype : str, optional
+        A precise dtype to use for fetching the results, by default 'default'
+    k : int, optional
+        The `k` in pass@k, by default 1
+    greedy : bool, optional
+        Whether we are computing pass@k for greedy decoding or not, by default True
     nan_values : str | None, optional
-        Representation of missing values, by default '-'
+        Optional str to fill missing values, by default '-'
 
     Returns
     -------
     pd.DataFrame
-        The model-wise pass@k.
+        A multi-column dataframe with the results.
     """
+    
+    benchs = all_passes_at_k_and_error_causes(dtype=dtype, k=k, greedy=greedy, to_df=True)
 
-    results = all_passes_at_k([k])
-        
-    # Swap to a "by model view"
-    models = set([x['model'] for x in results])
+    # Keep only needed columns and set the name as a tuple for later use MultiIndex
+    new_dfs = []
+    for df in benchs.values():
 
-    dics = []
-    for model in models:
+        dataset = df['dataset'][0]
+        mode = df['mode'][0]
+        context = str(df['use_context'][0])
 
-        # Find all results corresponding to current model
-        model_results = [x for x in results if x['model'] == model]
+        if dataset == 'HumanEval':
+            subindex = mode
+        else:
+            subindex = mode + '_' + context
 
-        model_family = model_results[0]['model_family']
-        model_size = model_results[0]['model_size']
-        dic = {'model': model, 'model_family': model_family, 'model_size': model_size}
+        new_df = df[['model', f'pass@{k}']].copy()
+        new_df[f'pass@{k}'] = new_df[f'pass@{k}']*100
+        new_df.rename(columns={f'pass@{k}': (dataset, subindex)}, inplace=True)
+        new_dfs.append(new_df)
 
-        for res in model_results:
-            dataset = res['dataset']
-            if dataset == 'HumanEval':
-                benchmark = res['mode']
-            else:
-                benchmark = res['mode'] + '_' + str(res['use_context'])
+    # Merge the dataframes together
+    final_df = new_dfs[0].merge(new_dfs[1], on='model', how='outer')
+    for df in new_dfs[2:]:
+        final_df = final_df.merge(df, on='model', how='outer')
 
-            dic[(dataset, benchmark)] = res[f'pass@{k}']*100
+    # Add the family and size to be able to sort after all merging (cannot keep the entries during merging)
+    final_df['model_family'] = [loader.ALL_MODELS_FAMILY[model] for model in final_df['model']]
+    final_df['model_size'] = [loader.ALL_MODELS_PARAMS[model] for model in final_df['model']]
+    # Sort and remove the family and size
+    final_df = final_df.sort_values(['model_family', 'model_size', 'model']).drop(columns=['model_family', 'model_size'])
+    # Set the model as index
+    final_df = final_df.set_index('model')
 
-        dics.append(dic)
-
-    # sort first according to model_family, then model size
-    df = pd.DataFrame.from_records(dics, index='model').sort_values(['model_family', 'model_size'])
-    df = df.drop(columns=['model_family', 'model_size'])
     # sort columns according to dataset, then put "generation" before "default"
-    columns = sorted(df.columns.to_list(), key=lambda x: (x[0], -len(x[1])))
-    df = df[columns]
-
+    columns = sorted(final_df.columns.to_list(), key=lambda x: (x[0], -len(x[1])))
+    final_df = final_df[columns]
     # use multi-column index
-    df.columns = pd.MultiIndex.from_tuples(df.columns)
-    if nan_values is not None:
-        df = df.fillna(nan_values)
+    final_df.columns = pd.MultiIndex.from_tuples(final_df.columns)
 
-    return df
+    if nan_values is not None:
+        final_df = final_df.fillna(nan_values)
+
+    return final_df
 
 
 
@@ -367,3 +539,9 @@ def latex(df: pd.DataFrame, **kwargs):
                         column_format='l|' + 'c'*len(df.columns), multicolumn_format='c',
                         index_names=False, **kwargs)
     print(table)
+
+
+
+def benchmark_error_causes(k: int = 1):
+
+    results = all_passes_at_k_and_error_causes([k])
