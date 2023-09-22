8dda91c0d69206a6a9c36412e23c78b7d4942542
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-22 16:32:47 +0200

    Update human_eval.sh

--- human_eval.sh
+++ human_eval.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=150G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=100G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:4
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
