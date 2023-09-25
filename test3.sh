ccf31d3053122187b2263da60177cdeb17dbe139
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-25 12:01:21 +0200

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=10G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:0
