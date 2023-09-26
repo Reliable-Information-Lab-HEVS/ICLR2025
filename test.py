bd9485fba8d2ed902b0f0c0e707c1f9aafb2973c
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-26 16:39:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -88,9 +88,9 @@ prompt = ' '.join(large_text.split(' ')[0:100])
 
 torch.cuda.empty_cache()
 
+torch.cuda.reset_peak_memory_stats(0)
 actual_peak = torch.cuda.max_memory_allocated(0) / 1024**3
 print(actual_peak)
-torch.cuda.reset_peak_memory_stats(0)
 print(torch.cuda.max_memory_allocated(0) / 1024**3)
 foo = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=0, seed=12)
 print(torch.cuda.max_memory_allocated(0) / 1024**3)
@@ -100,9 +100,9 @@ print(f'Mem : {mem} GiB')
 torch.cuda.empty_cache()
 gc.collect()
 
+torch.cuda.reset_peak_memory_stats(0)
 actual_peak2 = torch.cuda.max_memory_allocated(0) / 1024**3
 print(actual_peak2)
-torch.cuda.reset_peak_memory_stats(0)
 print(torch.cuda.max_memory_allocated(0) / 1024**3)
 foo2 = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=0, seed=12)
 print(torch.cuda.max_memory_allocated(0) / 1024**3)
@@ -111,9 +111,9 @@ mem2 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak2
 print(f'Mem with large max new tokens : {mem2} GiB')
 
 
+torch.cuda.reset_peak_memory_stats(0)
 actual_peak3 = torch.cuda.max_memory_allocated(0) / 1024**3
 print(actual_peak3)
-torch.cuda.reset_peak_memory_stats(0)
 print(torch.cuda.max_memory_allocated(0) / 1024**3)
 foo3 = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=0, seed=12)
 print(torch.cuda.max_memory_allocated(0) / 1024**3)
