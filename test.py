96b2c18c5d361ba10a0913d571fd2d623dc05629
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-27 10:45:34 +0200

    Update test.py

--- test.py
+++ test.py
@@ -86,13 +86,14 @@ model = engine.HFModel('star-chat-beta')
 model.extra_eos_tokens = []
 
 prompt = ' '.join(large_text.split(' ')[0:100])
+use_cache = True
 
 
 t0 = time.time()
 torch.cuda.reset_peak_memory_stats(0)
 actual_peak = torch.cuda.max_memory_allocated(0) / 1024**3
 foo = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=0, seed=12, post_process_output=False,
-            use_cache=False)
+            use_cache=use_cache)
 mem = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak
 dt0 = time.time() - t0
 
@@ -105,7 +106,7 @@ torch.cuda.reset_peak_memory_stats(0)
 actual_peak2 = torch.cuda.max_memory_allocated(0) / 1024**3
 print(actual_peak2)
 foo2 = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=5, seed=12, post_process_output=False,
-             use_cache=False)
+             use_cache=use_cache)
 print(torch.cuda.max_memory_allocated(0) / 1024**3)
 mem2 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak2
 dt1 = time.time() - t1
@@ -118,7 +119,7 @@ t2 = time.time()
 torch.cuda.reset_peak_memory_stats(0)
 actual_peak4 = torch.cuda.max_memory_allocated(0) / 1024**3
 foo4 = model(prompt, batch_size=1, max_new_tokens=200, min_new_tokens=200, seed=12, post_process_output=False,
-             use_cache=False)
+             use_cache=use_cache)
 mem4 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak4
 dt2 = time.time() - t2
 
@@ -142,7 +143,7 @@ t3 = time.time()
 torch.cuda.reset_peak_memory_stats(0)
 actual_peak5 = torch.cuda.max_memory_allocated(0) / 1024**3
 foo5 = model(new_prompt, batch_size=1, max_new_tokens=2, min_new_tokens=1, seed=12, post_process_output=False,
-             use_cache=False)
+             use_cache=use_cache)
 mem5 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak5
 dt3 = time.time() - t3
 
