080a45ed038c781cb818920c80029adb5e3091e3
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-27 12:24:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -304,13 +304,16 @@ model.model.__class__.sample = sample
 
 prompt = ' '.join(large_text.split(' ')[0:100])
 use_cache = False
+output_attentions = False
+output_hidden_states = False
+
 
 
 t0 = time.time()
 torch.cuda.reset_peak_memory_stats(0)
 actual_peak = torch.cuda.max_memory_allocated(0) / 1024**3
 foo = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=0, seed=12, post_process_output=False,
-            use_cache=use_cache)
+            use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
 mem = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak
 dt0 = time.time() - t0
 
@@ -322,7 +325,7 @@ t1 = time.time()
 torch.cuda.reset_peak_memory_stats(0)
 actual_peak2 = torch.cuda.max_memory_allocated(0) / 1024**3
 foo2 = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=5, seed=12, post_process_output=False,
-             use_cache=use_cache)
+             use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
 mem2 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak2
 dt1 = time.time() - t1
 
@@ -334,7 +337,7 @@ t2 = time.time()
 torch.cuda.reset_peak_memory_stats(0)
 actual_peak4 = torch.cuda.max_memory_allocated(0) / 1024**3
 foo4 = model(prompt, batch_size=1, max_new_tokens=200, min_new_tokens=200, seed=12, post_process_output=False,
-             use_cache=use_cache)
+             use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
 mem4 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak4
 dt2 = time.time() - t2
 
@@ -358,7 +361,7 @@ t3 = time.time()
 torch.cuda.reset_peak_memory_stats(0)
 actual_peak5 = torch.cuda.max_memory_allocated(0) / 1024**3
 foo5 = model(new_prompt, batch_size=1, max_new_tokens=2, min_new_tokens=1, seed=12, post_process_output=False,
-             use_cache=use_cache)
+             use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
 mem5 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak5
 dt3 = time.time() - t3
 
