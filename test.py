6e84d61456e875ac9d1bcd8571c205923bd4d415
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-28 11:54:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -403,7 +403,7 @@ with torch.no_grad():
     t0 = time.time()
     torch.cuda.reset_peak_memory_stats(0)
     actual_peak = torch.cuda.max_memory_allocated(0) / 1024**3
-    foo = model(prompt, batch_size=1, max_new_tokens=1, min_new_tokens=1, seed=12, post_process_output=False,
+    foo = model(prompt, batch_size=1, max_new_tokens=2, min_new_tokens=2, seed=12, post_process_output=False,
                 use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
     # foo = model.model(ids, use_cache=use_cache)
     # inputs = model.model.prepare_inputs_for_generation(concat_ids, use_cache=use_cache, past_key_values=past_keys1)
@@ -419,7 +419,7 @@ with torch.no_grad():
     t1 = time.time()
     torch.cuda.reset_peak_memory_stats(0)
     actual_peak2 = torch.cuda.max_memory_allocated(0) / 1024**3
-    foo2 = model(prompt, batch_size=1, max_new_tokens=1, min_new_tokens=1, seed=12, post_process_output=False,
+    foo2 = model(prompt, batch_size=1, max_new_tokens=2, min_new_tokens=2, seed=12, post_process_output=False,
                 use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
     # foo2 = model.model(ids, use_cache=use_cache)
     # inputs = model.model.prepare_inputs_for_generation(concat_ids, use_cache=use_cache, past_key_values=past_keys1)
@@ -451,7 +451,7 @@ with torch.no_grad():
     t3 = time.time()
     torch.cuda.reset_peak_memory_stats(0)
     actual_peak5 = torch.cuda.max_memory_allocated(0) / 1024**3
-    foo5 = model(new_prompt, batch_size=1, max_new_tokens=1, min_new_tokens=1, seed=12, post_process_output=False,
+    foo5 = model(new_prompt, batch_size=1, max_new_tokens=2, min_new_tokens=2, seed=12, post_process_output=False,
                 use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
     # foo5 = model.model(ids, use_cache=use_cache)
     # inputs = model.model.prepare_inputs_for_generation(concat_new_ids, use_cache=use_cache, past_key_values=past_keys2)
