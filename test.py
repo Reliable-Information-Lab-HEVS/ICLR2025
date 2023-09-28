cf020cc31dddfa86f022fd9837c4ed12a29af766
Cyril Vallez <cyril.vallez@gmail.com>
2023-09-28 09:26:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -321,6 +321,8 @@ past_keys = foo.past_key_values
 
 with torch.no_grad():
     ids = model.tokenizer.encode(prompt, return_tensors='pt').cuda()
+    bar = torch.tensor([[6483]]).cuda()
+    new_ids = torch.cat([ids, bar]).cuda()
 
 
 with torch.no_grad():
@@ -330,7 +332,7 @@ with torch.no_grad():
     # foo = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=0, seed=12, post_process_output=False,
     #             use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
     # foo = model.model(ids, use_cache=use_cache)
-    foo = model.model(torch.cat([ids, torch.tensor([[6483]], device=ids.device)]), use_cache=use_cache, past_key_values=past_keys)
+    foo = model.model(new_ids, use_cache=use_cache, past_key_values=past_keys)
     mem = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak
     dt0 = time.time() - t0
 
@@ -345,7 +347,7 @@ with torch.no_grad():
     # foo2 = model(prompt, batch_size=1, max_new_tokens=5, min_new_tokens=5, seed=12, post_process_output=False,
     #             use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
     # foo2 = model.model(ids, use_cache=use_cache)
-    foo2 = model.model(torch.cat([ids, torch.tensor([[6483]], device=ids.device)]), use_cache=use_cache, past_key_values=past_keys)
+    foo2 = model.model(new_ids, use_cache=use_cache, past_key_values=past_keys)
     mem2 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak2
     dt1 = time.time() - t1
 
@@ -360,7 +362,7 @@ with torch.no_grad():
     # foo4 = model(prompt, batch_size=1, max_new_tokens=500, min_new_tokens=500, seed=12, post_process_output=False,
     #             use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
     # foo4 = model.model(ids, use_cache=use_cache)
-    foo4 = model.model(torch.cat([ids, torch.tensor([[6483]], device=ids.device)]), use_cache=use_cache, past_key_values=past_keys)
+    foo4 = model.model(new_ids, use_cache=use_cache, past_key_values=past_keys)
     mem4 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak4
     dt2 = time.time() - t2
 
@@ -387,7 +389,7 @@ with torch.no_grad():
     # foo5 = model(new_prompt, batch_size=1, max_new_tokens=2, min_new_tokens=1, seed=12, post_process_output=False,
     #             use_cache=use_cache, output_attentions=output_attentions, output_hidden_states=output_hidden_states)
     # foo5 = model.model(ids, use_cache=use_cache)
-    foo5 = model.model(torch.cat([ids, torch.tensor([[6483]], device=ids.device)]), use_cache=use_cache, past_key_values=past_keys)
+    foo5 = model.model(new_ids, use_cache=use_cache, past_key_values=past_keys)
     mem5 = torch.cuda.max_memory_allocated(0) / 1024**3 - actual_peak5
     dt3 = time.time() - t3
 
