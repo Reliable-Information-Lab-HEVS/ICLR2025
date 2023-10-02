5519c7c9431af87022286bc97816cc874e366d42
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-02 14:09:34 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -5,18 +5,18 @@ import engine
 from helpers import utils
 
 
-# @utils.duplicate_function_for_gpu_dispatch
-# def test(model):
-#     try:
-#         foo = engine.HFModel(model)
-#     except Exception as e:
-#         print(f'Issue with {model}: {type(e).__name__}: {str(e)}')
-#         pass
+@utils.duplicate_function_for_gpu_dispatch
+def test(model):
+    try:
+        foo = engine.HFModel(model)
+    except Exception as e:
+        print(f'Issue with {model}: {type(e).__name__}: {str(e)}')
+        pass
 
-# if __name__ == '__main__':
-#     models = engine.SMALL_MODELS
-#     gpu_footprints = engine.estimate_number_of_gpus(models, False, False)
-#     utils.dispatch_jobs(gpu_footprints, torch.cuda.device_count(), test, models)
+if __name__ == '__main__':
+    models = engine.SMALL_MODELS
+    gpu_footprints = engine.estimate_number_of_gpus(models, False, False)
+    utils.dispatch_jobs(gpu_footprints, torch.cuda.device_count(), test, models)
 
-model = engine.HFModel('star-coder-plus')
+# model = engine.HFModel('star-coder-plus')
 
