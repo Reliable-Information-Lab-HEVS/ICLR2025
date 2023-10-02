1fbca4d9f4cfd505da6e925230b6f26f480acf20
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-02 10:41:53 +0200

    test

--- test3.py
+++ test3.py
@@ -4,11 +4,18 @@ from accelerate import init_empty_weights, load_checkpoint_and_dispatch, infer_a
 import time
 
 import engine
+from helpers import utils
 
 
-for model in engine.SMALL_MODELS:
+@utils.duplicate_function_for_gpu_dispatch
+def test(model):
     try:
         foo = engine.HFModel(model)
     except Exception as e:
         print(f'Issue with {model}: {type(e).__name__}: {str(e)}')
         pass
+
+models = engine.SMALL_MODELS
+gpu_footprints = engine.estimate_number_of_gpus(models, False, False)
+utils.dispatch_jobs(gpu_footprints, torch.cuda.device_count(), test, models)
+
