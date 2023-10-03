6e3eaea5ddea34a0a31d834e8faed931c7cde562
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-03 09:38:31 +0200

    test

--- engine/loader.py
+++ engine/loader.py
@@ -527,7 +527,7 @@ def estimate_model_gpu_footprint(model_name, quantization_8bits: bool = False, q
 
 def load_model(model_name: str, quantization_8bits: bool = False, quantization_4bits: bool = False,
                dtype: torch.dtype | None = None, max_fraction_gpu_0: float = 0.8, max_fraction_gpus: float = 0.8,
-               device_map: dict | None = None, gpu_rank: int = 0):
+               device_map: dict | str | None = None, gpu_rank: int = 0):
     """Load one of the supported pretrained model.
 
     Parameters
@@ -549,7 +549,7 @@ def load_model(model_name: str, quantization_8bits: bool = False, quantization_4
     max_fraction_gpus : float, optional
         The maximum fraction of the other gpus memory to reserve for the model. The default is 0.8. This is only
         used if `device_map` is `None`.
-    device_map : dict | None, optional
+    device_map : dict | str | None, optional
         The device map to decide how to split the model between available devices, by default None. If not
         provided, the model dispatch to GPU(s) is made according to `max_fraction_gpu_0` and `max_fraction_gpus`
         in such a way to use the smallest number of gpus that respect these two values.
@@ -575,11 +575,6 @@ def load_model(model_name: str, quantization_8bits: bool = False, quantization_4
     if quantization_8bits and quantization_4bits:
         raise ValueError(('You cannot load a model with both `quantization_8bits` and `quantization_4bits`. '
                          'Please choose one'))
-    
-    if isinstance(device_map, str):
-        raise ValueError(("You cannot provide the `device_map` as a string. Please use the `max_fraction_gpu_0` "
-                         "and `max_fraction_gpus` arguments to mimic `device_map='balanced'` or "
-                         "`device_map='balanced_low_0'` in a more coherent way."))
     
     # torch.float16 is not supported on cpu
     if not torch.cuda.is_available() and dtype != torch.float32:
@@ -621,7 +616,7 @@ def load_model(model_name: str, quantization_8bits: bool = False, quantization_4
 
         if min_gpu_needed > gpu_number:
             raise RuntimeError(("The model seems too big for the gpu resources you have. To offload to the cpu as well, "
-                               "explicitly pass a `device_map`, e.g device_map='balanced'."))
+                               "explicitly pass a `device_map`, e.g. device_map='balanced'."))
         
         # In this case we don't need a device_map, we just move the model to the 1st gpu. Most models are 
         # relatively small and should fall on this category.
--- test3.py
+++ test3.py
@@ -2,9 +2,13 @@ import torch
 import time
 
 import engine
+from engine import loader
 from helpers import utils
 
+model_name = 'bloom-176B'
 
-model = engine.HFModel('bloom-176B')
+min_gpu_needed, max_memory_map = loader.estimate_model_gpu_footprint(model_name)
+print(f'Min gpu needed: {min_gpu_needed}')
+model = engine.HFModel(model_name)
 print(model.get_gpu_memory_footprint())
 
