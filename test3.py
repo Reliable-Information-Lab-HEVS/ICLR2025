16c7c5fd55fa4f8b75a96492d48f870e81e7e0df
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-03 09:40:11 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -7,8 +7,6 @@ from helpers import utils
 
 model_name = 'bloom-176B'
 
-min_gpu_needed, max_memory_map = loader.estimate_model_gpu_footprint(model_name)
-print(f'Min gpu needed: {min_gpu_needed}')
-model = engine.HFModel(model_name)
+model = engine.HFModel(model_name, quantization_8bits=True)
 print(model.get_gpu_memory_footprint())
 
