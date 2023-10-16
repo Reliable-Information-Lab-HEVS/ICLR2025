362de3c2cf626a924ffa5dafe3c0edeb583d09c1
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-16 17:56:07 +0200

    Update test_srun.sh

--- test_srun.sh
+++ test_srun.sh
@@ -5,9 +5,9 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=80G
+#SBATCH --mem=100G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:titanv:3
+#SBATCH --gres=gpu:a100:3
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
