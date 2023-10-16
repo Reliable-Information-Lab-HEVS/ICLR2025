d0f072f55055d177f77985a89596d35a26dfdb9c
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-16 11:09:03 +0200

    test

--- test_allocator.py
+++ test_allocator.py
@@ -1,11 +1,11 @@
 import subprocess
 import time
 
-command = 'srun --gpus 1 --cpus-per-task 2 --mem 20G python3 test_srun.py'
+command = 'srun -ntasks 1 --gpus 1 --cpus-per-task 2 --mem 20G python3 test_srun.py'
 
 t0 = time.time()
 for i in range(3):
-    subprocess.run(command.split(' '))
-    # p = subprocess.run(['wrapper.sh', f'{i}'])
+    # subprocess.run(command.split(' '))
+    p = subprocess.run(['wrapper.sh', f'{i}'])
 dt = time.time() - t0
 print(f'Everything done in {dt:.2f} s')
--- wrapper.sh
+++ wrapper.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=$1
-srun --gpus 1 --cpus-per-task 2 --mem 20G python3 test_srun.py
+srun -ntasks 1 --gpus 1 --cpus-per-task 2 --mem 20G python3 test_srun.py
