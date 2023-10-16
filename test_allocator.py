4f092c7d04268fab6a9cb7979ba4222a7d91b550
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-16 11:03:13 +0200

    Update test_allocator.py

--- test_allocator.py
+++ test_allocator.py
@@ -4,8 +4,8 @@ import time
 command = 'srun --gpus 1 --cpus-per-task 2 --mem 20G python3 test_srun.py'
 
 t0 = time.time()
-for i in range(5):
-    # subprocess.run(command.split(' '))
-    p = subprocess.run(['wrapper.sh', f'{i}'])
+for i in range(3):
+    subprocess.run(command.split(' '))
+    # p = subprocess.run(['wrapper.sh', f'{i}'])
 dt = time.time() - t0
 print(f'Everything done in {dt:.2f} s')
