e328082fb66493e650dc882e265cd6f250a8df4b
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-16 14:38:56 +0200

    Update test_allocator.py

--- test_allocator.py
+++ test_allocator.py
@@ -10,14 +10,14 @@ command = 'srun --ntasks=1 --gpus=1 --gpu-bind=per_task:{gpus} --cpus-per-task=2
 
 t0 = time.time()
 processes = []
-for i in range(3):
+for i in range(5):
     # subprocess.run(command.format(gpus=1).split(' '))
     # p = subprocess.Popen([os.path.join(utils.ROOT_FOLDER, 'wrapper.sh'), f'{i}'])
     p = subprocess.Popen(command.format(gpus=1).split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     processes.append(p)
 
-# for p in processes:
-#     p.wait()
+for p in processes:
+    p.wait()
 
 dt = time.time() - t0
 print(f'Everything done in {dt:.2f} s')
