4cae067bd16d4adaf1f5a68b621ba56915d75a5d
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-16 10:36:51 +0200

    test

--- test_allocator.py
+++ test_allocator.py
@@ -5,6 +5,6 @@ command = 'srun --gpus 1 --cpus-per-task 2 --mem 20G python3 test_srun.py'
 
 t0 = time.time()
 for i in range(10):
-    subprocess.run(command)
+    subprocess.run(command.split(' '))
 dt = time.time() - t0
 print(f'Everything done in {dt:.2f} s')
