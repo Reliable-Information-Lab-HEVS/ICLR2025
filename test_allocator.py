437ad04a65e1474e10c5381bf4e0a6fa91c3f7de
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-16 12:15:14 +0200

    Update test_allocator.py

--- test_allocator.py
+++ test_allocator.py
@@ -6,11 +6,11 @@ from helpers import utils
 
 print(os.environ['CUDA_VISIBLE_DEVICES'])
 
-command = 'srun --ntasks 1 --gpus 1 --cpus-per-task 2 --mem 20G python3 test_srun.py'
+command = 'srun --ntasks 1 --gpus 1 --gpu-bind {gpus} --cpus-per-task 2 --mem 20G python3 test_srun.py'
 
 t0 = time.time()
 for i in range(3):
-    # subprocess.run(command.split(' '))
-    p = subprocess.Popen([os.path.join(utils.ROOT_FOLDER, 'wrapper.sh'), f'{i}'])
+    subprocess.run(command.format(gpus=i).split(' '))
+    # p = subprocess.Popen([os.path.join(utils.ROOT_FOLDER, 'wrapper.sh'), f'{i}'])
 dt = time.time() - t0
 print(f'Everything done in {dt:.2f} s')
