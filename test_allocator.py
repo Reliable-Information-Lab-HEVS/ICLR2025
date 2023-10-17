9995fd866b3c7788b689757542a85dd359447486
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-17 16:18:47 +0200

    test

--- test_allocator.py
+++ test_allocator.py
@@ -13,12 +13,12 @@ t0 = time.time()
 # num_gpus = torch.cuda.device_count()
 num_gpus = 5
 
-models = ['bloom-560M']*5
+models = ['bloom-560M']*3
 
 print(f'Launching computations with {num_gpus} gpus available.')
 
 # gpu_footprints = engine.estimate_number_of_gpus(models, False, False)
-gpu_footprints = [1]*5
+gpu_footprints = [1]*3
 commands = [f'-v python3 -u test_srun.py' for _ in models]
 
 utils.dispatch_jobs_srun(gpu_footprints, num_gpus, commands, cpus_per_task=1, memory=3)
--- test_srun.sh
+++ test_srun.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=21G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:titanv:5
+#SBATCH --gres=gpu:v100:3
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
