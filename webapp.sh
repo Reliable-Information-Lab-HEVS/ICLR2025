2a507112ae7beeea4e3e0ceeb515eed1e15b341d
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-17 10:18:44 +0200

    Update webapp.sh

--- webapp.sh
+++ webapp.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=50G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:4
+#SBATCH --gres=gpu:a100:2
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
