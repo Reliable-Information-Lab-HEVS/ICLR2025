034470e1504ea025551f3c16d14cb94581e9ca7e
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-17 16:45:10 +0200

    Update test_allocator.py

--- test_allocator.py
+++ test_allocator.py
@@ -19,7 +19,7 @@ print(f'Launching computations with {num_gpus} gpus available.')
 
 # gpu_footprints = engine.estimate_number_of_gpus(models, False, False)
 gpu_footprints = [1]*3
-commands = [f'-v python3 -u test_srun.py' for _ in models]
+commands = [f'--exclusive -v python3 -u test_srun.py' for _ in models]
 
 utils.dispatch_jobs_srun(gpu_footprints, num_gpus, commands, cpus_per_task=1, memory=3)
 
