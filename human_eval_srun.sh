2bcaf82c8ac3a0745d07e6a4fa305e987ffea508
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-17 17:53:29 +0200

    test

--- human_eval_srun.sh
+++ human_eval_srun.sh
@@ -19,6 +19,9 @@ conda activate llm
 # make sure the job scheduler is using very few resources that are not the same as the subprocesses
 # that will be launched
 # srun --ntasks=1 --gpus-per-task=0 --cpus-per-task=1 --mem=5G python3 -u human_eval_wrapper.py "$@"
+
+# How are the resources of this python script managed? Because we can still spawn child processes correctly
+# even with exclusive option
 python3 -u human_eval_wrapper.py "$@"
 
 conda deactivate
