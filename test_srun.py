525abcddc666f98249781918cc9bbfe3b56c4e41
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-17 16:42:01 +0200

    test

--- test_srun.py
+++ test_srun.py
@@ -19,13 +19,12 @@ from helpers import utils, datasets
 
 
 def test():
+    t0 = time.time()
     model_name = 'bloom-560M'
     print(f'Starting with {model_name}')
     model = engine.HFModel(model_name)
 
     dataset = datasets.HumanEval()
-
-    t0 = time.time()
     for sample in dataset[0:30]:
         out = model(sample['prompt'], prompt_template_mode='generation')
     dt = time.time() - t0
