63f559c4cad8fd193983cfb7da0bc1f901510628
Cyril Vallez <cyril.vallez@gmail.com>
2023-10-19 11:21:36 +0200

    test

--- test3.py
+++ test3.py
@@ -3,11 +3,28 @@ import time
 
 import engine
 from engine import loader
-from helpers import utils
+from helpers import utils, datasets
 
-model_name = 'bloom-176B'
+HUMAN_EVAL_GENERATION_KWARGS = {
+    'max_new_tokens': 512,
+    'min_new_tokens': 5,
+    'do_sample': True,
+    'top_k': None,
+    'top_p': 0.95,
+    'num_return_sequences': 200,
+    'seed': 1234,
+    'truncate_prompt_from_output': True,
+}
 
-model = engine.HFModel(model_name, quantization_8bits=True, max_fraction_gpu_0=0.6,
-                       max_fraction_gpus=0.6)
-print(model.get_gpu_memory_footprint())
+model_name = 'codegen-16B'
 
+model = engine.HFModel(model_name)
+data = datasets.HumanEvalPHP()
+prompt = data[0]['prompt']
+stop = data[0]['stop_tokens']
+
+t0 = time.time()
+completions = model(prompt, temperature=0.8, stopping_patterns=stop, prompt_template_mode='generation',
+                    **HUMAN_EVAL_GENERATION_KWARGS)
+dt = time.time() - t0
+print(f'It took {dt:.2f} s')
--- test3.sh
+++ test3.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=5
-#SBATCH --mem=80G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=35G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:8
+#SBATCH --gres=gpu:a100:1
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
