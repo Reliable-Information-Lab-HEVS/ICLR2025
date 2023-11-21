f26bb442967ac9308d6d9252aa0dd3cfdba78ffa
Cyril Vallez <cyril.vallez@gmail.com>
2023-11-21 11:37:28 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -679,4 +679,4 @@ with demo:
 
 
 if __name__ == '__main__':
-    demo.queue().launch(share=True, auth=authentication, blocked_paths=[CREDENTIALS_FILE])
+    demo.queue(concurrency_count=4).launch(share=True, auth=authentication, blocked_paths=[CREDENTIALS_FILE])
