82a7d6aa805d6c6e6d7aeb04fb8273dcf7c84369
Cyril Vallez <cyril.vallez@gmail.com>
2023-12-05 15:05:37 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -5,7 +5,7 @@ import engine
 from engine import loader, stopping
 from helpers import utils, datasets
 
-HUMAN_EVAL_GENERATION_KWARGS = {
+GENERATION_KWARGS = {
     'max_new_tokens': 10,
     'min_new_tokens': 0,
     'do_sample': True,
@@ -17,27 +17,39 @@ HUMAN_EVAL_GENERATION_KWARGS = {
     # 'truncate_prompt_from_output': True,
 }
 
-# model_name = 'llama2-7B-chat'
-model_name = 'vicuna-7B'
+# # model_name = 'llama2-7B-chat'
+# model_name = 'vicuna-7B'
 
-model = engine.HFModel(model_name)
-prompt = 'Hello there, could you tell me what is the meaning of life?'
+# model = engine.HFModel(model_name)
+# prompt = 'Hello there, could you tell me what is the meaning of life?'
 
-# t0 = time.time()
-# completions = model(prompt, temperature=0.8, **HUMAN_EVAL_GENERATION_KWARGS)
-# dt = time.time() - t0
-# print(f'It took {dt:.2f} s')
+# # t0 = time.time()
+# # completions = model(prompt, temperature=0.8, **GENERATION_KWARGS)
+# # dt = time.time() - t0
+# # print(f'It took {dt:.2f} s')
 
-prompt = model.format_prompt(prompt)
-input = model.tokenizer.encode(prompt, return_tensors='pt')
-input_length = input.shape[-1]
-if torch.cuda.is_available():
-    input = input.to(device=model.input_device)
+# prompt = model.format_prompt(prompt)
+# input = model.tokenizer.encode(prompt, return_tensors='pt')
+# input_length = input.shape[-1]
+# if torch.cuda.is_available():
+#     input = input.to(device=model.input_device)
 
-outputs = model.model.generate(input, stopping_criteria=None, **HUMAN_EVAL_GENERATION_KWARGS)
-outputs = outputs[0].tolist()
-truncated_outputs = outputs[input_length:]
+# outputs = model.model.generate(input, stopping_criteria=None, **GENERATION_KWARGS)
+# outputs = outputs[0].tolist()
+# truncated_outputs = outputs[input_length:]
 
-print(f'prompt_tokens: {outputs[:input_length]}')
-print(f'completion_ids: {truncated_outputs}')
-print(f'completion_tokens: {model.tokenizer.convert_ids_to_tokens(truncated_outputs)}')
+# print(f'prompt_tokens: {outputs[:input_length]}')
+# print(f'completion_ids: {truncated_outputs}')
+# print(f'completion_tokens: {model.tokenizer.convert_ids_to_tokens(truncated_outputs)}')
+
+
+models = list(loader.CODEGEN2_MODELS_MAPPING.keys())
+for model in models:
+
+    model = engine.HFModel(model)
+    prompt = 'Hello there, could you tell me what is the meaning of life?'
+
+    try:
+        out = model(prompt, **GENERATION_KWARGS)
+    except BaseException as e:
+        print(f'{model}: {repr(e)}')
