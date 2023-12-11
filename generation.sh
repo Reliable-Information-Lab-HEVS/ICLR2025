6e888a64e03343cb4d1b6d3e8f314295718f04f8
Cyril Vallez <cyril.vallez@gmail.com>
2023-12-11 18:40:14 +0100

    Create generation.sh

---
+++ generation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=generation
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=10-00:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20G
+#SBATCH --partition=nodes
+#SBATCH --gres=gpu:a100:1
+#SBATCH --chdir=/cluster/raid/home/vacy/LLMs
+
+# Initialize the shell to use local conda
+eval "$(conda shell.bash hook)"
+
+# Activate (local) env
+conda activate llm-playground
+
+python3 generation.py "$@"
+
+conda deactivate
