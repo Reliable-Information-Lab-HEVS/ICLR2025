8ba60f77f493945afdb248045e2092d1fc16f89c
Cyril Vallez <cyril.vallez@gmail.com>
2023-12-11 15:31:27 +0100

    test

--- test4.sh
+++ test4.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=100M
+#SBATCH --mem-per-cpu=10G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:1
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
