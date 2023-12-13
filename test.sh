cc526ac17ded2c81950aadf831a05a2a3ab8188e
Cyril Vallez <cyril.vallez@gmail.com>
2023-12-13 09:53:35 +0100

    Clean-up tests

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=20G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:1
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
@@ -14,7 +14,7 @@
 eval "$(conda shell.bash hook)"
 
 # Activate (local) env
-conda activate llm
+conda activate llm-playground
 
 python3 test.py "$@"
 
