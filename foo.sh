8c0a1ee36186ba68903cd8081da010a08ca284b1
Cyril Vallez <cyril.vallez@gmail.com>
2024-02-22 15:02:45 +0100

    Update foo.sh

--- foo.sh
+++ foo.sh
@@ -16,7 +16,8 @@ eval "$(conda shell.bash hook)"
 # Activate (local) env
 conda activate test
 
-srun --exclusive --exact --ntasks=1 --cpus-per-task=1 --mem=1G ../frp_server/frp_0.54.0_linux_amd64/frpc -c ../frp_server/frpc/frpc.toml
+# srun --exclusive --exact --ntasks=1 --cpus-per-task=1 --mem=1G ../frp_server/frp_0.54.0_linux_amd64/frpc -c ../frp_server/frpc/frpc.toml &
+../frp_server/frp_0.54.0_linux_amd64/frpc -c ../frp_server/frpc/frpc.toml &
 
 # ss -tulpn
 python3 foo.py 
