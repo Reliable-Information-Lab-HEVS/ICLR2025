b5c4195667cc60c81a5f0bdb16f4fe703cfb25dd
Cyril Vallez <cyril.vallez@gmail.com>
2024-02-28 18:13:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,11 @@ from transformers import AutoModelForCausalLM
 from TextWiz import textwiz
 from TextWiz.memory_estimator import LARGE_TEXT
 
-# model = textwiz.HFModel('zephyr-7B-beta')
-model = AutoModelForCausalLM.from_pretrained('HuggingFaceH4/zephyr-7b-beta', torch_dtype=torch.bfloat16,
+model = textwiz.HFModel('zephyr-7B-beta')
+new_model = AutoModelForCausalLM.from_pretrained('HuggingFaceH4/zephyr-7b-beta', torch_dtype=torch.bfloat16,
                                              low_cpu_mem_usage=True, attn_implementation='sdpa')
 # model = model.to_bettertransformer()
+model.model = new_model
 model('Hello please do your magic', num_return_sequences=1, batch_size=1, max_new_tokens=2)
 
 large_tokens = model.tokenizer.encode(LARGE_TEXT)
