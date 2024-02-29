fed3cd89e0997669770e4f93c80e3579d165aec2
Cyril Vallez <cyril.vallez@gmail.com>
2024-02-29 10:49:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ for input_size in sizes:
         torch.cuda.reset_peak_memory_stats()
         actual_peak = torch.cuda.max_memory_allocated() / 1024**3
 
-        with torch.backends.cuda.sdp_kernel(enable_flash=False, enable_math=False, enable_mem_efficient=True):
+        with torch.backends.cuda.sdp_kernel(enable_flash=False, enable_math=True, enable_mem_efficient=False):
             foo = model(prompt, num_return_sequences=1, batch_size=1, max_new_tokens=2)
 
         memory_used = (torch.cuda.max_memory_allocated() / 1024**3) - actual_peak
