6aaa23becebccc218be1862c21b99ff93496d628
Cyril Vallez <cyril.vallez@gmail.com>
2024-02-29 11:10:26 +0100

    add test

---
+++ test2.py
@@ -0,0 +1,46 @@
+
+import time
+import numpy as np
+import torch
+from transformers import AutoModelForCausalLM
+
+from TextWiz import textwiz
+from TextWiz.memory_estimator import LARGE_TEXT
+
+model = textwiz.HFModel('zephyr-7B-beta')
+new_model = AutoModelForCausalLM.from_pretrained('HuggingFaceH4/zephyr-7b-beta', torch_dtype=torch.bfloat16,
+                                             low_cpu_mem_usage=True, attn_implementation='sdpa').cuda()
+# model = model.to_bettertransformer()
+model.model = new_model
+model('Hello please do your magic', num_return_sequences=1, batch_size=1, max_new_tokens=2)
+
+large_tokens = model.tokenizer.encode(LARGE_TEXT)
+sizes = [10, 100, 1000, 2000, 4000]
+N = 10
+
+times = {}
+memories = {}
+
+for input_size in sizes:
+    prompt = model.tokenizer.decode(large_tokens[:input_size], skip_special_tokens=True)
+    gen_times = []
+    gen_mem = []
+
+    for i in range(N):
+        t0 = time.time()
+        torch.cuda.reset_peak_memory_stats()
+        actual_peak = torch.cuda.max_memory_allocated() / 1024**3
+
+        with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
+            foo = model(prompt, num_return_sequences=1, batch_size=1, max_new_tokens=4024-input_size, min_new_tokens=4024-input_size)
+
+        memory_used = (torch.cuda.max_memory_allocated() / 1024**3) - actual_peak
+
+        gen_times.append(time.time() - t0)
+        gen_mem.append(memory_used)
+
+    times[input_size] = np.mean(gen_times)
+    memories[input_size] = np.mean(gen_mem)
+
+print(f'time: {times}')
+print(f'memory: {memories}')
---
+++ test2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=test
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --time=10-00:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=10G
+#SBATCH --partition=nodes
+#SBATCH --gres=gpu:a100:1
+#SBATCH --chdir=/cluster/raid/home/vacy/LLMs
+
+# Initialize the shell to use local conda
+eval "$(conda shell.bash hook)"
+
+# Activate (local) env
+# conda activate llm-playground
+conda activate test
+
+python3 test2.py "$@"
+
+conda deactivate
