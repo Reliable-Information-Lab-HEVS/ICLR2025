753c49c8df750e76816029aeca17cd8a4c764bdb
Cyril Vallez <cyril.vallez@gmail.com>
2024-03-05 16:34:29 +0100

    test

--- test.py
+++ test.py
@@ -9,17 +9,17 @@ from TextWiz import textwiz
 from TextWiz.memory_estimator import LARGE_TEXT
 
 # model = textwiz.HFModel('llama2-70B-chat')
-model = textwiz.HFModel('code-llama-7B-instruct')
-model.model = None
-gc.collect()
-torch.cuda.empty_cache()
+model = textwiz.HFModel('code-llama-34B-instruct')
+# model.model = None
+# gc.collect()
+# torch.cuda.empty_cache()
 # model.model = torch.compile(model.model)
-new_model = AutoModelForCausalLM.from_pretrained('codellama/CodeLlama-34b-Instruct-hf', torch_dtype=torch.bfloat16,
-                                             low_cpu_mem_usage=True, attn_implementation='sdpa', device_map='auto')
-new_tokenizer = textwiz.load_tokenizer('code-llama-34B-instruct')
+# new_model = AutoModelForCausalLM.from_pretrained('codellama/CodeLlama-34b-Instruct-hf', torch_dtype=torch.bfloat16,
+                                            #  low_cpu_mem_usage=True, attn_implementation='sdpa', device_map='auto')
+# new_tokenizer = textwiz.load_tokenizer('code-llama-34B-instruct')
 # model = model.to_bettertransformer()
-model.model = new_model
-model.tokenizer = new_tokenizer
+# model.model = new_model
+# model.tokenizer = new_tokenizer
 model('Hello please do your magic', num_return_sequences=1, batch_size=1, max_new_tokens=2)
 
 large_tokens = model.tokenizer.encode(LARGE_TEXT)
