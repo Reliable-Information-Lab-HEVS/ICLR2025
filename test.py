43c24068b7d6e51af99f61765c4433a297825b17
Cyril Vallez <cyril.vallez@gmail.com>
2024-03-05 12:53:38 +0100

    test

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from transformers import AutoModelForCausalLM
 from TextWiz import textwiz
 from TextWiz.memory_estimator import LARGE_TEXT
 
-model = textwiz.HFModel('code-llama-34B-instruct')
-model.model = torch.compile(model.model)
+model = textwiz.HFModel('llama2-70B-chat')
+# model.model = torch.compile(model.model)
 # new_model = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', torch_dtype=torch.bfloat16,
 #                                              low_cpu_mem_usage=True, attn_implementation='sdpa').cuda()
 # model = model.to_bettertransformer()
