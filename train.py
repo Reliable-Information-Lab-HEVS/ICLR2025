480dad6fce2be47e842d497c3b0662cf2dab741d
Cyril Vallez <cyril.vallez@gmail.com>
2024-06-10 17:55:50 +0200

    Update train.py

--- train.py
+++ train.py
@@ -3,23 +3,22 @@ from TextWiz.textwiz import loader
 
 from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
 
-training_args = TrainingArguments(
-    optim='adamw_torch',
-    per_device_train_batch_size=3,
-    learning_rate=5e-5,
-    num_train_epochs=5,
-    bf16=True,
-    dataloader_num_workers=4,
-    torch_compile=True,
-    output_dir='training_results',
-    logging_strategy='epoch',
-    save_strategy='epoch',
-    report_to='tensorboard',
-    seed=123,
-)
-
-
 def main():
+
+    training_args = TrainingArguments(
+        optim='adamw_torch',
+        per_device_train_batch_size=3,
+        learning_rate=5e-5,
+        num_train_epochs=5,
+        bf16=True,
+        dataloader_num_workers=4,
+        torch_compile=True,
+        output_dir='training_results',
+        logging_strategy='epoch',
+        save_strategy='epoch',
+        report_to='tensorboard',
+        seed=123,
+    )
 
     model_name = 'llama3-8B-instruct'
     model, tokenizer = loader.load_model_and_tokenizer(model_name)
@@ -28,11 +27,11 @@ def main():
     collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
 
     trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=dataset,
-    data_collator=collator,
-    tokenizer=tokenizer
+        model=model,
+        args=training_args,
+        train_dataset=dataset,
+        data_collator=collator,
+        tokenizer=tokenizer
     )
 
     trainer.train()
