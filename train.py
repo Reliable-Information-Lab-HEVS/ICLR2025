6859e809fcac2b31f984b4496b697193d8623b2e
Cyril Vallez <cyril.vallez@gmail.com>
2024-06-10 18:14:29 +0200

    update train

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 import os
 
-from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from transformers import AutoModelForCausalLM, Trainer, TrainingArguments, DataCollatorForLanguageModeling
 
 from helpers import datasets
 from TextWiz.textwiz import loader
 
-def main():
+model_name = 'llama3-8B-instruct'
 
-    training_args = TrainingArguments(
+training_args = TrainingArguments(
         optim='adamw_torch',
         per_device_train_batch_size=3,
         learning_rate=5e-5,
@@ -20,11 +20,18 @@ def main():
         save_strategy='epoch',
         report_to='tensorboard',
         seed=123,
-        local_rank=int(os.environ["LOCAL_RANK"]),
     )
 
-    model_name = 'llama3-8B-instruct'
-    model, tokenizer = loader.load_model_and_tokenizer(model_name)
+
+def main():
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        attn_implementation='flash_attention_2',
+        torch_dtype=loader.get_model_dtype(model_name),
+        low_cpu_mem_usage=True
+    )
+    tokenizer = loader.load_tokenizer(model_name)
 
     dataset = datasets.WalliserDeutschDataset(tokenizer, sample_size=loader.get_model_context_size(model_name))
     collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
@@ -37,10 +44,10 @@ def main():
         tokenizer=tokenizer
     )
 
-    trainer.train()
+    training_results = trainer.train()
+    # print(training_results)
 
 
 if __name__ == '__main__':
 
-    print(int(os.environ["LOCAL_RANK"]))
     main()
