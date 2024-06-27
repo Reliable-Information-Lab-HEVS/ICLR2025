525fac97de1be46da706c6eda41c52bae7cbd873
Cyril Vallez <cyril.vallez@gmail.com>
2024-06-27 15:45:27 +0200

    Update TextWiz dev branch

--- AATK_instruct.sh
+++ AATK_instruct.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=11
 #SBATCH --mem=150G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:5
+#SBATCH --gres=gpu:a100:6
 #SBATCH --chdir=/cluster/raid/home/vacy/LLMs
 
 # Initialize the shell to use local conda
