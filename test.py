e0125b963caebf135c6009bf4def21cee77f2e6f
Cyril Vallez <cyril.vallez@gmail.com>
2024-07-03 14:33:42 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,22 @@
 from TextWiz import textwiz
 
 import torch
+from transformers import AutoModelForCausalLM
 import time
 
 model = textwiz.HFCausalModel('llama3-8B-instruct')
+model = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
+                                             torch_dtype=textwiz.loader.ALL_MODELS_DTYPES['llama3-8B-instruct'],
+                                             attn_implementation='sdpa', low_cpu_mem_usage=True).cuda()
 
 prompt = 'Write an extremely long text about monkeys'
 
 # Default mode
 t0 = time.time()
-foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, batch_size=1)
+foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, batch_size=1,
+            cache_implementation='static')
 dt0 = time.time() - t0
-print(f'Total time for base inference: {dt0:.2e} s --- {dt0 / 4096:.2f} tokens/s')
+print(f'Total time for base inference: {dt0:.2e} s --- {4096 / t0:.2f} tokens/s')
 
 # Default compiling
 t0 = time.time()
@@ -23,7 +28,7 @@ t0 = time.time()
 foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, batch_size=1,
             cache_implementation='static')
 dt0 = time.time() - t0
-print(f'Total time for inference: with default compiling {dt0:.2e} s --- {dt0 / 4096:.2f} tokens/s')
+print(f'Total time for inference: with default compiling {dt0:.2e} s --- {4096 / t0:.2f} tokens/s')
 
 
 # Better compiling
@@ -36,4 +41,4 @@ t0 = time.time()
 foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, batch_size=1,
             cache_implementation='static')
 dt0 = time.time() - t0
-print(f'Total time for inference: with autotune compiling {dt0:.2e} s --- {dt0 / 4096:.2f} tokens/s')
+print(f'Total time for inference: with autotune compiling {dt0:.2e} s --- {4096 / t0:.2f} tokens/s')
