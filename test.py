37f95d2fbe98382cdf1eff1bad7a602ad79329a2
Cyril Vallez <cyril.vallez@gmail.com>
2024-07-03 14:52:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,13 @@ from transformers import AutoModelForCausalLM
 import time
 
 model = textwiz.HFCausalModel('llama3-8B-instruct')
-model_ = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
+model1 = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
                                              torch_dtype=textwiz.loader.ALL_MODELS_DTYPES['llama3-8B-instruct'],
                                              attn_implementation='sdpa', low_cpu_mem_usage=True).cuda()
-model.model = model_
+model2 = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
+                                             torch_dtype=textwiz.loader.ALL_MODELS_DTYPES['llama3-8B-instruct'],
+                                             attn_implementation='sdpa', low_cpu_mem_usage=True).cuda()
+model.model = model1
 
 prompt = 'Write an extremely long text about monkeys'
 
@@ -17,7 +20,7 @@ t0 = time.time()
 foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, batch_size=1,
             cache_implementation='static')
 dt0 = time.time() - t0
-print(f'Total time for base inference: {dt0:.2e} s --- {4096 / t0:.2f} tokens/s')
+print(f'Total time for base inference: {dt0:.2e} s --- {4096 / dt0:.2f} tokens/s')
 
 # Default compiling
 t0 = time.time()
@@ -29,9 +32,9 @@ t0 = time.time()
 foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, batch_size=1,
             cache_implementation='static')
 dt0 = time.time() - t0
-print(f'Total time for inference: with default compiling {dt0:.2e} s --- {4096 / t0:.2f} tokens/s')
+print(f'Total time for inference: with default compiling {dt0:.2e} s --- {4096 / dt0:.2f} tokens/s')
 
-
+model.model = model2
 # Better compiling
 t0 = time.time()
 model.model = torch.compile(model.model, mode='max-autotune')
@@ -42,4 +45,4 @@ t0 = time.time()
 foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, batch_size=1,
             cache_implementation='static')
 dt0 = time.time() - t0
-print(f'Total time for inference: with autotune compiling {dt0:.2e} s --- {4096 / t0:.2f} tokens/s')
+print(f'Total time for inference: with autotune compiling {dt0:.2e} s --- {4096 / dt0:.2f} tokens/s')
