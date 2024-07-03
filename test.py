9fd3312aeaeb3d8cae91b6a338368b4ed59580f7
Cyril Vallez <cyril.vallez@gmail.com>
2024-07-03 15:02:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,10 @@ from transformers import AutoModelForCausalLM
 import time
 
 model = textwiz.HFCausalModel('llama3-8B-instruct')
-model1 = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
+model_ = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
                                              torch_dtype=textwiz.loader.ALL_MODELS_DTYPES['llama3-8B-instruct'],
                                              attn_implementation='sdpa', low_cpu_mem_usage=True).cuda()
-model2 = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
-                                             torch_dtype=textwiz.loader.ALL_MODELS_DTYPES['llama3-8B-instruct'],
-                                             attn_implementation='sdpa', low_cpu_mem_usage=True).cuda()
-model.model = model1
+model.model = model_
 
 prompt = 'Write an extremely long text about monkeys'
 
@@ -34,7 +31,10 @@ foo = model(prompt, max_new_tokens=4096, min_new_tokens=4096, do_sample=False, b
 dt0 = time.time() - t0
 print(f'Total time for inference: with default compiling {dt0:.2e} s --- {4096 / dt0:.2f} tokens/s')
 
-model.model = model2
+model_ = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
+                                             torch_dtype=textwiz.loader.ALL_MODELS_DTYPES['llama3-8B-instruct'],
+                                             attn_implementation='sdpa', low_cpu_mem_usage=True).cuda()
+model.model = model_
 # Better compiling
 t0 = time.time()
 model.model = torch.compile(model.model, mode='max-autotune')
