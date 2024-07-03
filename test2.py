c728bd0b7c27a63f89635635016dcc3b5c4a79e6
Cyril Vallez <cyril.vallez@gmail.com>
2024-07-03 18:41:19 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,7 +6,7 @@ import time
 import os
 
 os.environ['TORCH_LOGS' ] = "+dynamo"
-os.environ['TORCHDYNAMO_VERBOSE'] = 1
+os.environ['TORCHDYNAMO_VERBOSE'] = "1"
 
 model = AutoModelForCausalLM.from_pretrained(textwiz.loader.ALL_MODELS_MAPPING['llama3-8B-instruct'],
                                              torch_dtype=textwiz.loader.ALL_MODELS_DTYPES['llama3-8B-instruct'],
