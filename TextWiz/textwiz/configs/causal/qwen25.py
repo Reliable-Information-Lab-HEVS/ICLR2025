80e710c0616628936083265988fa5df5baa3afec
alexandersternfeld <alex.sternfeld@live.nl>
2025-07-07 11:20:40 +0200

    debug

--- TextWiz/textwiz/configs/causal/qwen25.py
+++ TextWiz/textwiz/configs/causal/qwen25.py
@@ -11,5 +11,5 @@ MODELS_MAPPING = {
 }
 MODELS_DTYPES = {model: torch.bfloat16 for model in MODELS_MAPPING.keys()}
 MODELS_PARAMS = _infer_model_sizes(MODELS_MAPPING)
-MODELS_FAMILY = {model: 'qwen2.5' for model in MODELS_MAPPING.keys()}
+MODELS_FAMILY = {model: 'qwen25' for model in MODELS_MAPPING.keys()}
 MODELS_CONTEXT_SIZE = {model: 32768 for model in MODELS_MAPPING.keys()}
